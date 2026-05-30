4f57004899f7f0cb91bde057aaf5b4a2f51ad810
cemal-ozcan <115502713+cemal-ozcan@users.noreply.github.com>
2026-05-30 23:05:37 +0300

    feat(detectors): SensorFrozen kuralı (süre/universal, Faz 4 Iter 4.2)

--- src/detectors/rules/__init__.py
+++ src/detectors/rules/__init__.py
@@ -8,6 +8,7 @@ from detectors.rules.hydraulic_pressure_decline import HydraulicPressureDecline
 from detectors.rules.motor_current_high import MotorCurrentHigh
 from detectors.rules.motor_temperature_high import MotorTemperatureHigh
 from detectors.rules.motor_voltage_erratic import MotorVoltageErratic
+from detectors.rules.sensor_frozen import SensorFrozen
 from detectors.rules.vibration_elevated import VibrationElevated
 
 # Callable[..., Detector]: kurallar farklı __init__ imzalı; build_detectors generic
@@ -17,5 +18,6 @@ RULE_REGISTRY: dict[str, Callable[..., Detector]] = {
     "motor_current_high": MotorCurrentHigh,
     "motor_temperature_high": MotorTemperatureHigh,
     "motor_voltage_erratic": MotorVoltageErratic,
+    "sensor_frozen": SensorFrozen,
     "vibration_elevated": VibrationElevated,
 }
